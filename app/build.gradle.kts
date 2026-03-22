efb318a9cf2b11542fbefd727dfe40b036d33882
Saksham Raturi <raturisaksham2@gmail.com>
2026-03-22 15:46:11 +0530

    app v1 basic p2p achieved using firebase, RTC, and room ids

---
+++ app/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    alias(libs.plugins.android.application)
+        id("com.google.gms.google-services")
+
+}
+
+
+android {
+    namespace = "com.example.p2pm"
+    compileSdk {
+        version = release(36)
+    }
+
+    sourceSets {
+        getByName("main") {
+            jniLibs.srcDirs("libs")
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.example.p2pm"
+        minSdk = 21
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(files("libs/libwebrtc.aar"))
+    implementation("com.google.firebase:firebase-database:20.3.0")
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
